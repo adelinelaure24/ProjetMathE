2b1fe51861f7f3bdec88fdaff97256863ea6a60d
adelinelaure24 <lactouoadeline188@gmail.com>
2025-03-10 09:32:48 +0100

    Update MathsEprojet.py

--- MathsEprojet.py
+++ MathsEprojet.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+st.title("Mon application Streamlit")
+st.write("Ceci est une application simple avec Streamlit.")
+
+# Ajouter un champ de saisie
+user_input = st.text_input("Entrez quelque chose :")
+st.write(f"Vous avez entré : {user_input}")
+
+# Ajouter un bouton
+if st.button("Cliquez-moi"):
+    st.write("Bouton cliqué !")
 
